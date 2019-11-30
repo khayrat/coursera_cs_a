bcf3877688243c3118b2e14ccc4b3ee781a2cf17
Khayrat Glende <khayrat.glende@neofonie.de>
2019-11-30 14:35:31 +0100

    added ActivationFunciton

--- 5/ass_05/ActivationFunction.java
+++ 5/ass_05/ActivationFunction.java
@@ -11,38 +11,44 @@ public class ActivationFunction {
     // Returns the sigmoid function of x.
     public static double sigmoid(double x) {
       if (Double.isNaN(x)) return Double.NaN;
-      return 1/(1+Math.exp(-x));
+      return 1.0/(1.0+Math.exp(-x));
     }
 
     // Returns the hyperbolic tangent of x.
     public static double tanh(double x) {
       if (Double.isNaN(x)) return Double.NaN;
+      //if x≥20, then tanh(x) should return 1.0; 
+      //if x≤−20, then tanh(x) should return -1.0.
+      if      (x >= 20) return  1.0;
+      else if (x <= 20) return -1.0;
       return (Math.exp(x) - Math.exp(-x))/(Math.exp(x)+Math.exp(-x));
     }
 
     // Returns the softsign function of x.
     public static double softsign(double x) {
       if (Double.isNaN(x)) return Double.NaN;
-      return x/(1+Math.abs(x));
+      if       (x == Double.POSITIVE_INFINITY) return  1.0;
+      else if  (x == Double.NEGATIVE_INFINITY) return -1.0;
+      return x/(1.0+Math.abs(x));
     }
 
     // Returns the square nonlinearity function of x.
     public static double sqnl(double x) {
       if (Double.isNaN(x)) return Double.NaN;
-      if (x <= -2) return -1;
+      if (x <= -2) return -1.0;
       if (x <   0) return x + x*x/4;
       if (x <   2) return x - x*x/4;
-      else /*x >= 2*/ return 1;
+      else /*x >= 2*/ return 1.0;
     }
 
     // Takes a double command-line argument x and prints each activation
     // function, evaluated, in the format (and order) given below.
     public static void main(String[] argv) {
       double x = Double.parseDouble(argv[0]);
-      StdOut.printf("heaviside(%f) = %f\n", x, heaviside(x));
-      StdOut.printf("sigmoid(%f) = %f\n", x, sigmoid(x));
-      StdOut.printf("tanh(%f) = %f\n", x, tanh(x));
-      StdOut.printf("softsign(%f) = %f\n", x, softsign(x));
-      StdOut.printf("sqnl(%f) = %f\n", x, sqnl(x));
+      StdOut.printf("heaviside(%s) = %s\n", x, heaviside(x));
+      StdOut.printf("sigmoid(%s) = %s\n", x, sigmoid(x));
+      StdOut.printf("tanh(%s) = %s\n", x, tanh(x));
+      StdOut.printf("softsign(%s) = %s\n", x, softsign(x));
+      StdOut.printf("sqnl(%s) = %s\n", x, sqnl(x));
     }
 }
