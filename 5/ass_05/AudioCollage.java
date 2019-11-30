a848dab2b83797486b248bd59cf7505bfbf4c66d
Khayrat Glende <khayrat.glende@neofonie.de>
2019-11-30 15:29:45 +0100

    AudioCollage

--- 5/ass_05/AudioCollage.java
+++ 5/ass_05/AudioCollage.java
@@ -2,28 +2,33 @@ public class AudioCollage {
 
     // Returns a new array that rescales a[] by a multiplicative factor of alpha.
     public static double[] amplify(double[] a, double alpha) {
+      return null;
 
     }
 
     // Returns a new array that is the reverse of a[].
     public static double[] reverse(double[] a) {
 
+      return null;
     }
 
     // Returns a new array that is the concatenation of a[] and b[].
     public static double[] merge(double[] a, double[] b) {
 
+      return null;
     }
 
     // Returns a new array that is the sum of a[] and b[],
     // padding the shorter arrays with trailing 0s if necessary.
     public static double[] mix(double[] a, double[] b) {
 
+      return null;
     }
 
     // Returns a new array that changes the speed by the given factor.
     public static double[] changeSpeed(double[] a, double alpha) {
 
+      return null;
     }
 
     // Creates an audio collage and plays it on standard audio.
