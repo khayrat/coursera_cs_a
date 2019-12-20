ef174b4498218d7598255f9cc466b97598d7138f
Khayrat Glende <khayrat.glende@neofonie.de>
2019-12-20 14:55:52 +0100

    progress.

--- 8/ass_08/KernelFilter.java
+++ 8/ass_08/KernelFilter.java
@@ -1,9 +1,37 @@
 public class KernelFilter {
+    private static double[][] gausianKernel = null;
+
+    private static double[][] getGaussianKernel() {
+      if (gausianKernel == null)
+      {
+        double[][] kernel = {
+          {1, 2, 1},
+          {2, 4, 2},
+          {1, 2, 1}
+        };
+
+        for (int r = 0; r < 3; r++)
+          for (int c = 0; c < 3; c++)
+            kernel[r][c] = 1./16 * kernel[r][c];
+
+        gausianKernel = kernel;
+      }
+      return gausianKernel;
+    }
+
+    private static Picture applyFilter(Picture picture, double[][] kernel)
+    {
+      int h = picture.height();
+      int w = picture.width();
+      Picture pic = new Picture(w, h);
+    
+      return pic;
+    }
 
     // Returns a new picture that applies a Gaussian blur filter to the given picture.
     public static Picture gaussian(Picture picture)
     {
-      return null;
+      return applyFilter(picture, getGaussianKernel());
     }
 
     // Returns a new picture that applies a sharpen filter to the given picture.
@@ -33,6 +61,8 @@ public class KernelFilter {
     // Test client (ungraded).
     public static void main(String[] args)
     {
+      Picture pic = new Picture(args[0]);
+
 
     }
 }
